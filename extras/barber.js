f8722390ba934a3616d1a4eeb35cc2d8aaba01e4
RajabDildar <rajabdildar4678@gmail.com>
2024-10-10 21:25:08 +0500

    initial commit

---
+++ extras/barber.js
@@ -0,0 +1,4 @@
+// Update contact information dynamically
+document.getElementById("phone").textContent = "Your Phone Number";
+document.getElementById("address").textContent =
+  "Ghulam Muhammad Abad, Faisalabad, Punjab, Pakistan";
