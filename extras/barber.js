fae57cec85a70de6dff05457e4b7629f577e7250
RajabDildar <rajabdildar4678@gmail.com>
2024-12-13 14:49:42 +0500

    deleted barber files

--- extras/barber.js
+++
@@ -1,4 +0,0 @@
-// Update contact information dynamically
-document.getElementById("phone").textContent = "Your Phone Number";
-document.getElementById("address").textContent =
-  "Ghulam Muhammad Abad, Faisalabad, Punjab, Pakistan";
